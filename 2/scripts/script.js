9ce8b872586eff9cfbb350478c26cc4d44c0cd4c
Esteban Ibañez <139912376+mancomanXD777@users.noreply.github.com>
2026-04-15 20:14:22 -0400

    commit

---
+++ 2/scripts/script.js
@@ -0,0 +1,21 @@
+function entrar() {
+    var u = document.getElementById("usuarioInput").value;
+    var c = document.getElementById("claveInput").value;
+    var errorDiv = document.getElementById("mensajeError");
+
+    // Limpiamos el mensaje previo
+    errorDiv.innerHTML = "";
+
+    if (u === "esñtp" && c === "Tokyo") {
+        window.location.href = "index3.html";
+    } 
+    else if (u !== "esñtp" && c === "Tokyo") {
+        errorDiv.innerHTML = "Error: Tradúzcala al español donde se agrega la Ñ.";
+    } 
+    else if (u === "esñtp" && c !== "Tokyo") {
+        errorDiv.innerHTML = "Error: Traduce el código de forma correcta.";
+    } 
+    else {
+        errorDiv.innerHTML = "Datos no concordantes. Intente de nuevo.";
+    }
+}
