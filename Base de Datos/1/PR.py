d2cf564576181f1cea09946f4d91b66d96511e24
mancomanXD777 <estebanibanez@liceovvh.cl>
2026-04-30 17:44:32 -0400

    commit

--- Base de Datos/1/PR.py
+++ Base de Datos/1/PR.py
@@ -7,3 +7,18 @@ db = cliente['Tienda_LevelUp']
 colecction = db['Catalogo_productos']
 
 
+consola = {
+    "nombre": "Nintendo",
+    "Marca": "nintendo",
+    "Precio": 3500,
+    "Stock": 10,
+    "especificaciones": {
+        "pantalla": "oled 7 pulgada",
+        "color": "neon"
+        }
+
+}
+
+resultado = colecction.insert_one(consola)
+
+print (f"producto insertado con id: {resultado.inserted_id}")
